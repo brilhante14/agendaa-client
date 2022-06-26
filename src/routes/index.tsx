36b287ba636cbdf19819f75c533b374f4475d397
Felipe Keiler <36575953+fkeiler@users.noreply.github.com>
2022-06-26 00:01:32 +0000

    Adicionada rota de turmas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,17 +8,19 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { ForgotPassword } from "../pages/ForgotPassword";
+import Turmas from "../pages/Turmas";
 
 // Renderer
 export default function UserRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/forgotPassword" element={<ForgotPassword />} />
-                <Route path="*" element={<Navigate to="/login" replace />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}   
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/turmas" element={<Turmas />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
