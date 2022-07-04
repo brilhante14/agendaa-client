a1454a2eb13d8ff4d892c61f1adb18d3e434a40d
Felipe Keiler <36575953+fkeiler@users.noreply.github.com>
2022-07-04 19:33:04 +0000

    Adicionada rota para turma selecionada

---
+++ src/pages/NenhumaTurma/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import thinking from "../../assets/thinking.png";
+
+const NenhumaTurma = () => {
+  return (
+    <>
+      <img src={thinking} alt="" />
+      <h3>Nenhuma Turma Selecionada</h3>
+      <h4>Selecione uma para ver os detalhes</h4>
+    </>
+  );
+};
+
+export default NenhumaTurma;
