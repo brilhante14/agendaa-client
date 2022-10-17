b56d480fd43f785bfcfaefcc795dc1bba23f482a
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2022-10-17 09:49:44 -0300

    feat: Updating dns

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-   baseURL: 'http://3.94.201.196:5000/'
+const api = axios.create({ 
+   baseURL: 'http://35.175.151.163:5000/'
 });
 
 export default api;  
